4279fd818a9d42a4e799948c311afc0afdfdecfe
sky790312 <sky790312yks@gmail.com>
2024-07-22 02:17:23 +0800

    feat: use hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MainChoiceView from '../views/MainChoiceView.vue'
 import SymptomChoiceView from '../views/SymptomChoiceView.vue'
 import QuestionView from '../views/QuestionView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
